667177b9fe25dfb25f3ceabf1c56926e35889b44
Masianuy <Masianuy@ukr.net>
2023-09-04 17:26:23 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Projects from './routes/Projects';
+import Contacts from './routes/Contacts';
+import PageNotFound from './routes/PageNotFound';
 import './App.css';
 
 function App () {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contacts' element={<Contacts />} />
+          <Route path='/*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
